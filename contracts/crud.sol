ed7f9fe72017f36292d39199d6c0830c245b4656
Afolabi Sunday <sundayafolabi992@gmail.com>
2024-01-12 23:44:57 +0000

    Update crud.sol
    updated file to include update and delete

--- contracts/crud.sol
+++ contracts/crud.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-
-
 pragma solidity 0.8.22;
 
 /*
@@ -15,93 +13,142 @@ Solidty smart contract account that accept username and password
 - code is only available on remix ide 
 */
 
+import "hardhat/console.sol";
 
 contract CRUDWITHLOGIN {
     struct User {
         address owner;
         bool isConnected;
-        string password;
+        bytes32 password;
         string username;
         bool isUser; // used to check if this is a valid user we set, it will always be true for valid user
     }
-    mapping (address => User) public userMapping;
+    mapping(address => User) private  userMapping;
 
+    //create a modifer that checks the if the msg.sender is connected
 
-    //create a modifer that checks the if the msg.sender is connected 
-
-    modifier isUserConnected (){
+    modifier isUserConnected() {
         bool userStatus = userMapping[msg.sender].isConnected;
         require(userStatus == true, "This user is not connected");
         _;
     }
 
     // only owner should be able to query their own details using modifer
-    modifier onlyOwner (address caller){
-        require(msg.sender == caller, "Not authorized to view data");
+    modifier onlyOwner() {
+        console.log(userMapping[msg.sender].owner);
+        address owner = userMapping[msg.sender].owner;
+        require(msg.sender == owner, "Not authorized to view data");
         _;
     }
 
     //check if a user exist withing our mapping.
 
-    modifier userExist(){
+    modifier userExist() {
         require(userMapping[msg.sender].isUser == true, "user does not exist");
         _;
     }
     // add a login event
-    event LoginSuccessfully (string description, bool loginstatus);
+    event LoginSuccessfully(string description, bool loginstatus);
 
     //logout event
-    event LogoutSuccessfully (string description, bool logoutStatus);
+    event LogoutSuccessfully(string description, bool logoutStatus);
 
-    //signup event 
-    event SignupSuccessfully ( string description, string username, bool status);
+    //signup event
+    event SignupSuccessfully(string description, string username, bool status);
 
+    //incorrect password
+    event IncorrectPassword(string message, string status, string statusCode);
 
     //custom error
 
-    error unAuthorized();
-
-
+    error unAuthorized(string message);
 
     // query user details
-    function getMydetails(address userAddress) public view isUserConnected onlyOwner(userAddress) returns (User memory) {
-      User memory data = userMapping[userAddress];
-      return data;
+    function getMydetails()
+        public
+        view
+        isUserConnected
+        onlyOwner
+        returns (User memory)
+    {
+        User memory data = userMapping[msg.sender];
+        return data;
     }
 
-    //login users 
-    function login(string memory _username, string memory password) public userExist returns(bool status){
+    //login users
+    function login(string memory _password)
+        public
+        userExist
+        returns (bool status)
+    {
         User memory user = userMapping[msg.sender];
-        if (keccak256(abi.encodePacked(user.username)) == keccak256(abi.encodePacked(user.password))) {
-             User({ owner:msg.sender, isConnected:true, password:password, username:_username, isUser:true});
-             emit LoginSuccessfully("user successfully logged in", true);
+        if (
+            user.password ==
+            keccak256(abi.encodePacked(_password))
+        ) {
+            userMapping[msg.sender].isConnected = true;
+            emit LoginSuccessfully("user successfully logged in", true);
             return true;
-        } else if (keccak256(abi.encodePacked(user.username)) == keccak256(abi.encodePacked(user.password))) {
-            revert unAuthorized();
+        } else {
+            revert unAuthorized("the password you entered is incorrect");
         }
     }
 
-
     //signup function
-    function signup(string memory username, string memory password) public returns (bool) {
-       User memory user = User({ owner:msg.sender, isConnected:true, password:password, username:username, isUser:true}); //password should be hashed using keccak256
-       userMapping[msg.sender] = user; 
+    function signup(string memory username, string memory password)
+        public
+        returns (bool)
+    {
+        User memory user = User({
+            owner: msg.sender,
+            isConnected: true,
+            password: keccak256(abi.encodePacked(password)),
+            username: username,
+            isUser: true
+        }); //password should be hashed using keccak256
+        userMapping[msg.sender] = user;
         emit SignupSuccessfully("successfully signup", username, true);
-       return true;
+        return true;
     }
 
-
     //protected route
-    function protectedFunction() public view isUserConnected  returns (string memory){
+    function protectedFunction()
+        public
+        view
+        isUserConnected
+        returns (string memory)
+    {
         string memory resp = string.concat("user is ", " fully authenticated");
         return resp;
     }
 
     //logout function
-    function logout() public isUserConnected returns (string memory){
+    function logout() public isUserConnected returns (string memory) {
         userMapping[msg.sender].isConnected = false;
-        string memory loggedOut = "You have successfully logout from the protocol";
+        string
+            memory loggedOut = "You have successfully logout from the protocol";
         emit LogoutSuccessfully("successfully Loggedout", true);
         return loggedOut;
+    }
+
+    //update user details
+    // user exist
+    function update(string memory username, string memory password)
+        public
+        isUserConnected
+        onlyOwner
+        userExist
+        returns (bool)
+    {
+        userMapping[msg.sender].username = username;
+        userMapping[msg.sender].password = keccak256(abi.encodePacked(password));
+        userMapping[msg.sender].isConnected = false; //logout if connected
+
+        return true;
+    }
+
+    function remove() public isUserConnected userExist returns (bool){
+        delete userMapping[msg.sender];
+        return true;
     }
 }
